5dd08bde1607c5beb7342577a983902dcacc71b2
José Fresco <josemariafresco@gmail.com>
2017-03-02 10:10:04 -0300

    Calculate enqueue time with more precision

--- src/helpers.js
+++ src/helpers.js
@@ -1,3 +1,5 @@
+import microtime from 'microtime'
+
 /**
  * Gets a keyword describing an HTTP status code
  *
@@ -32,12 +34,18 @@ export const getRequestDuration = startTime => new Date().getTime() - startTime
 
 /**
  * Gets the time in ms since the request has been enqueued in the web server.
- * It is assumed that the `queueHeader` has the value in the format `t=123` where `123` is the time in ms
- * since epoch.
+ * It is assumed that the `queueHeader` has the value in the format `t=123456.7890` where `123456.7890` is the
+ * time in ms since epoch.
  *
  * @return {Number} The time in ms since the request has been enqueued
  */
 
-export const getQueueDuration = (req, queueHeader) => req.header(queueHeader)
-  ? new Date().getTime() - (+req.header(queueHeader).replace('t=', ''))
-  : null
+export const getQueueDuration = (req, queueHeader) => {
+  if (!req.header(queueHeader)) {
+    return
+  }
+
+  const queueTime = +req.header(queueHeader).replace('t=', '')
+  const now = microtime.nowDouble()
+  return now - queueTime
+}
